14f03145975e12b5fdf5ea7d970cc6f8823d9c7a
Justin Tulloss <justin.tulloss@gmail.com>
2013-06-08 16:03:24 -0700

    Set up package and grunt file

---
+++ client/Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      // define the files to lint
+      files: ['Gruntfile.js', '*.js'],
+      // configure JSHint (documented at http://www.jshint.com/docs/)
+      options: {
+          // more options here if you want to override JSHint defaults
+        globals: {
+          console: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      options: {
+        interrupt: true
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          hostname: "*"
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('dev', ['connect', 'watch']);
+};
